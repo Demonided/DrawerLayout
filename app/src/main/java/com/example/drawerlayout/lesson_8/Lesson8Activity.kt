b178f096e4d8c614ea6a4567fe88daa97d0b01a9
Demonided <Demonided@mail.ru>
2023-04-05 17:49:39 +0300

    lesson 8 wery good

--- app/src/main/java/com/example/drawerlayout/lesson_8/Lesson8Activity.kt
+++ app/src/main/java/com/example/drawerlayout/lesson_8/Lesson8Activity.kt
@@ -2,6 +2,10 @@ package com.example.drawerlayout.lesson_8
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.drawerlayout.R
 import com.example.drawerlayout.databinding.ActivityLesoon8Binding
 
 class Lesson8Activity : AppCompatActivity() {
@@ -11,5 +15,31 @@ class Lesson8Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLesoon8Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Maks Shostak"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.lesson_8_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            android.R.id.home -> finish()
+            R.id.syncNow -> {
+                Toast.makeText(this, "Sync", Toast.LENGTH_SHORT).show()
+            }
+            R.id.save -> {
+                Toast.makeText(this, "Save", Toast.LENGTH_SHORT).show()
+            }
+            R.id.delete -> {
+                Toast.makeText(this, "Delete", Toast.LENGTH_SHORT).show()
+            }
+            R.id.search -> {
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
