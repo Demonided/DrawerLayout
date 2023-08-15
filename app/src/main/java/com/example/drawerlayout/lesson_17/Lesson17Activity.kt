ea7788194125a04cc0129ce38a804d90566c7047
Demonided <Demonided@mail.ru>
2023-08-15 16:57:03 +0300

    SDAw

---
+++ app/src/main/java/com/example/drawerlayout/lesson_17/Lesson17Activity.kt
@@ -0,0 +1,12 @@
+package com.example.drawerlayout.lesson_17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drawerlayout.R
+
+class Lesson17Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson17)
+    }
+}
---
+++ app/src/main/java/com/example/drawerlayout/lesson_19/Lesson19Activity.kt
@@ -0,0 +1,12 @@
+package com.example.drawerlayout.lesson_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drawerlayout.R
+
+class Lesson19Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson19)
+    }
+}
